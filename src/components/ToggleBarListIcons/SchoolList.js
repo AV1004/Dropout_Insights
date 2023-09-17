68be0195f317787d0aa0258efe0a7da3de778b28
Valay Andhariya <andhariyavalay4@gmail.com>
2023-09-18 03:03:24 +0530

    Updating Togglebar List Styles

--- src/components/ToggleBarListIcons/SchoolList.js
+++ src/components/ToggleBarListIcons/SchoolList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SchoolList() {
+export default function SchoolList(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,53 +8,53 @@ export default function SchoolList() {
       height="34"
       viewBox="0 0 34 34"
       fill="none"
-      className="bg-[#267DFF] h-6 mr-4 mt-1"
+      className={`bg-[${props.changeBG}] h-6 mr-4 mt-1`}
     >
       <path
         d="M31.45 8.55484L26.5478 30.2571C26.1987 31.8772 24.8896 33 23.3767 33H4.26284C2.06634 33 0.495353 30.6259 1.14993 28.3001L7.27394 6.61405C7.69578 5.10628 8.96134 4.06352 10.3869 4.06352H28.2789C29.6608 4.06352 30.8099 4.99387 31.29 6.27708C31.5663 6.9668 31.6245 7.75283 31.45 8.55484Z"
         stroke="#FEFEFE"
-        stroke-width="2"
-        stroke-miterlimit="10"
+        strokeWidth="2"
+        strokeMiterlimit="10"
       />
       <path
         d="M22.824 33H29.7771C31.6536 33 33.1228 31.2516 32.9919 29.1825L31.5518 7.33578"
         stroke="#FEFEFE"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M13.6307 7.9453L15.1435 1.01605"
         stroke="#FEFEFE"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M23.3768 7.96136L24.7441 1"
         stroke="#FEFEFE"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.7505 16.9599H22.3876"
         stroke="#FEFEFE"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.2959 23.3759H20.933"
         stroke="#FEFEFE"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- src/components/ToggleBarListItems.js
+++ src/components/ToggleBarListItems.js
@@ -6,54 +6,220 @@ import Register from "./ToggleBarListIcons/Register";
 import Login from "./ToggleBarListIcons/Login";
 
 export default function ToggleBarListItems() {
-  const [bgColorDashboard, setBgColorDashboard] = useState("#267DFF");
-  const [changePaddingDashboard, setChangePaddingDashboard] = useState("px-10");
-  const [AddClassDashboard, setAddClassDashboard] = useState("bg-[#267DFF]");
+  const [dashboard, setDashboard] = useState({
+    bgColor: "#267DFF",
+    padding: "px-10",
+    class: "bg-[#267DFF]",
+  });
+  const [schoolList, setSchoolList] = useState({
+    bgColor: "#267DFF",
+    padding: "px-10",
+    class: "bg-[#267DFF]",
+  });
+  const [analysis, setAnalysis] = useState({
+    bgColor: "#267DFF",
+    padding: "px-10",
+    class: "bg-[#267DFF]",
+  });
+  const [register, setRegister] = useState({
+    bgColor: "#267DFF",
+    padding: "px-10",
+    class: "bg-[#267DFF]",
+  });
+  const [login, setLogin] = useState({
+    bgColor: "#267DFF",
+    padding: "px-10",
+    class: "bg-[#267DFF]",
+  });
 
   const handleDashboard = () => {
-    setBgColorDashboard("#DBE7FD");
-    setChangePaddingDashboard("pl-10");
-    setAddClassDashboard(
-      "bg-[#DBE7FD] w-[13.9rem] p-4 rounded-tl-3xl rounded-bl-3xl"
-    );
+    setDashboard({
+      bgColor: "#DBE7FD",
+      padding: "pl-10",
+      class: "bg-[#DBE7FD] w-[13.9rem] p-4 rounded-tl-3xl rounded-bl-3xl",
+    });
+    setSchoolList({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setAnalysis({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setRegister({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setLogin({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+  };
+  const handleSchoolList = () => {
+    setSchoolList({
+      bgColor: "#DBE7FD",
+      padding: "pl-10",
+      class: "bg-[#DBE7FD] w-[13.9rem] p-4 rounded-tl-3xl rounded-bl-3xl",
+    });
+    setDashboard({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setAnalysis({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setRegister({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setLogin({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+  };
+  const handleAnalysis = () => {
+    setAnalysis({
+      bgColor: "#DBE7FD",
+      padding: "pl-10",
+      class: "bg-[#DBE7FD] w-[13.9rem] p-4 rounded-tl-3xl rounded-bl-3xl",
+    });
+    setDashboard({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setSchoolList({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setRegister({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setLogin({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+  };
+  const handleRegister = () => {
+    setRegister({
+      bgColor: "#DBE7FD",
+      padding: "pl-10",
+      class: "bg-[#DBE7FD] w-[13.9rem] p-4 rounded-tl-3xl rounded-bl-3xl",
+    });
+    setDashboard({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setSchoolList({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setAnalysis({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setLogin({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+  };
+  const handleLogin = () => {
+    setLogin({
+      bgColor: "#DBE7FD",
+      padding: "pl-10",
+      class: "bg-[#DBE7FD] w-[13.9rem] p-4 rounded-tl-3xl rounded-bl-3xl",
+    });
+    setDashboard({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setSchoolList({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setAnalysis({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
+    setRegister({
+      bgColor: "#267DFF",
+      padding: "px-10",
+      class: "bg-[#267DFF]",
+    });
   };
 
   return (
-    <div className="bg-[#267DFF] h-[29.8rem] rounded-br-3xl">
+    <div className="bg-[#267DFF] h-[29.8rem]  2xl:h-[35rem] rounded-br-3xl">
       <ul>
         <li
-          className={`bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium  ${changePaddingDashboard} flex py-6`}
+          className={`bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium  ${dashboard.padding} flex py-6`}
         >
-          <div className={AddClassDashboard}>
+          <div className={dashboard.class}>
             <button onClick={handleDashboard} className="flex">
-              <Dashboard changeBG={bgColorDashboard} />
+              <Dashboard changeBG={dashboard.bgColor} />
               {"Dashboard"}
             </button>
           </div>
         </li>
-        <li className="bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium px-10 flex py-6">
-          <button className="flex">
-            <SchoolList />
-            {"School List"}
-          </button>
+        <li
+          className={`bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium ${schoolList.padding} flex py-6`}
+        >
+          <div className={schoolList.class}>
+            <button onClick={handleSchoolList} className="flex">
+              <SchoolList changeBG={schoolList.bgColor} />
+              {"School List"}
+            </button>
+          </div>
         </li>
-        <li className="bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium px-10 flex py-6">
-          <button className="flex">
-            <Analysis />
-            {"Analysis"}
-          </button>
+        <li
+          className={`bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium ${analysis.padding} flex py-6`}
+        >
+          <div className={analysis.class}>
+            <button onClick={handleAnalysis} className="flex">
+              <Analysis changeBG={analysis.bgColor} />
+              {"Analysis"}
+            </button>
+          </div>
         </li>
-        <li className="bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium px-10 flex py-6">
-          <button className="flex">
-            <Register />
-            {"Register"}
-          </button>
+        <li
+          className={`bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium ${register.padding} flex py-6`}
+        >
+          <div className={register.class}>
+            <button onClick={handleRegister} className="flex">
+              <Register changeBG={register.bgColor} />
+              {"Register"}
+            </button>
+          </div>
         </li>
-        <li className="bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium px-10 flex py-6">
-          <button className="flex">
-            <Login />
-            {"Login"}
-          </button>
+        <li
+          className={`bg-[#267DFF] text-[#FFFFFF] text-2xl font-medium ${login.padding} flex py-6`}
+        >
+          <div className={login.class}>
+            <button onClick={handleLogin} className="flex">
+              <Login changeBG={login.bgColor} />
+              {"Login"}
+            </button>
+          </div>
         </li>
       </ul>
     </div>
